e84fe5ec6b0db651d2665189ec3b78146ac48255
Dinārs Kemzāns 221RDB321 <125220545+DinaarsKemzaans@users.noreply.github.com>
2023-03-16 18:25:41 +0000

    ??

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
 #Dinārs Kemzāns 17. grupa 221RDB321
 
+import heapq
 def parallel_processing(n, m, data):
     output = []
+    maxval =[(0,i) for i in range(n)]
+    for x in data:
+        beigsana, numurs = heapq.heappop(maxval)
+        output.append((numurs, beigsana))
+
+        beigsana += x
+        heapq.heappush(maxval, (beigsana, numurs))
+
 
     return output
 
 def main():
-    n = 0
-    m = 0
-    data = []
-
+    data = list(map(int, input().split()))
+    n = data[0]
+    m = data[1]
+    data = list(map(int, input().split()))
+    assert len(data) == m
     # TODO: create the function
     result = parallel_processing(n,m,data)
     
     for x in range(len(result)):
-        print(result[x][0] + " " + result[x][1])
+        print(str(result[x][0]) + " " + str(result[x][1]))
 
 if __name__ == "__main__":
     main()
